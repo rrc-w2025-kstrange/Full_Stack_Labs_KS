96ee016636ae58e8fe1ba2595f5b78d5b88dbb72
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-09-23 22:18:06 -0500

    Add the header content to Nav

---
+++ src/components/common/nav/Nav.tsx
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <header>
+      <img src="https://itsm-ace.ca/images/logo.svg" alt="Pixell River Logo" width={120} />
+      <div>
+        <h1>Pixell River Employee Directory</h1>
+        <p>Welcome to the employee portal</p>
+      </div>
+    </header>
+  );
+}
+
+export default Nav;
